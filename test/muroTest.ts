e2186a34659cea6f71fdc20a12ce1bded569fe28
Augusto Portillo <aportillo@desarrollosnea.com>
2019-04-27 14:53:40 -0300

    Test, Agregar Publicación Privada

--- test/muroTest.ts
+++ test/muroTest.ts
@@ -10,17 +10,9 @@ describe("Agregar Publicación", () => {
     usuario.publicar("Publicación Pública", "Esto es un contenido.", true);
     expect(usuario.muro.publicaciones[0].espublico).equal(true);
   });
-  //   it("No se debería agregar un album con el mismo nombre", () => {
-  //     const usuario = new Usuario("correo@ucp.edu.ar");
-  //     usuario.agregarAlbum("Album1");
-  //     usuario.agregarAlbum("Album1");
-  //     expect(usuario.albums.length).equal(2);
-  //   });
-  //   it("Agregar 10 albums con nombre AlbumX", () => {
-  //     const usuario = new Usuario("correo@ucp.edu.ar");
-  //     for (let index = 0; index < 10; index++) {
-  //       usuario.agregarAlbum(`Album ${index}`);
-  //     }
-  //     expect(usuario.albums.length).equal(11);
-  //   });
+  it("Se debería agregar una publicación Privada", () => {
+    const usuario = new Usuario("correo@ucp.edu.ar");
+    usuario.publicar("Publicación Privada", "Esto es un contenido.");
+    expect(usuario.muro.publicaciones[0].espublico).equal(false);
+  });
 });
